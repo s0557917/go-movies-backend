e0674e8c1e9fe242fc83de0207121cbec87266bf
Phillip Friedel <s0557917@htw-berlin.de>
2023-01-25 13:30:02 +0100

    Added basic web server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	//set application config
+	var app application
+
+	//read from command line
+
+	//connect to database
+
+	//start a webserver
+	app.Domain = "example.com"
+
+	log.Println("Starting server on port", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
